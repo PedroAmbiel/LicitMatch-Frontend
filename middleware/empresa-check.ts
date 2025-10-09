d798bcf6439bd2661da5e32c6e51d3de40a818ab
PedroAmbiel <pedro.am4@puccampinas.edu.br>
2025-10-09 19:12:47 -0300

    correções

--- middleware/empresa-check.ts
+++ middleware/empresa-check.ts
@@ -3,17 +3,15 @@ import { userStore } from "#imports"
 export default defineNuxtRouteMiddleware((to, from) => {
 const userStoreImport = userStore()
 const uiStore = useUiStore()
-  
-  console.log('rodei')
+
   if (to.path.startsWith('/main')  && userStoreImport.isAutenticado()) {
     if (to.path === '/main/cadastro_empresa') {
       return
     }
 
     if (!userStoreImport.verificarSeEstaVinculadoAEmpresa()) {
-      if (from.path !== to.path) {
+      if (!from.path.startsWith('/public') && from.path !== to.path) {
         setTimeout(() => {
-          
           uiStore.abrirDialogEmpresaRequerida()
         }, 100)
         
